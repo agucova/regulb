9d9fbd21a40951d52aba379bb0aa617921ac64b8
Agustín Covarrubias <agucova@uc.cl>
2021-04-22 22:10:14 -0400

    ✅ mvp

--- src/public/app/api/apirouter.php
+++ src/public/app/api/apirouter.php
@@ -9,12 +9,7 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 require(dirname(dirname(dirname(dirname(__FILE__)))) . '/vendor/autoload.php');
 
 $data = json_decode(file_get_contents("php://input"));
-if (!$data->test) {
-  $api = new ApiRouter();
-  $response = $api->ejecutar($data);
-  echo json_encode($response);
-} else {
-  echo json_encode($data);
-}
-
+$api = new ApiRouter();
+$response = $api->ejecutar($data);
+echo json_encode($response);
 exit;
