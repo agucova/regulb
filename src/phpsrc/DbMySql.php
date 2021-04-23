b36b1d559f8c82615c4874d13cbcd2d6bb3e074e
Agustín Covarrubias <agucova@uc.cl>
2021-04-23 00:45:41 -0400

    add_key_exists

--- src/phpsrc/DbMySql.php
+++ src/phpsrc/DbMySql.php
@@ -127,7 +127,7 @@ abstract class DbMySql
             // Create connection
             $config = new Config();
             // Check the environment for an RDS database. 
-            if (getenv($_SERVER['RDS_HOSTNAME'])) {
+            if (array_key_exists('RDS_HOSTNAME', $_SERVER)) {
                 $conn = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
             } else {
                 $conn = mysqli_connect(
